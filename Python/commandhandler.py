d374fe5ff3fe7053c4c672f6cb206f7a790cbd06
Loewenzahn <christoph@hbothe.de>
2025-04-05 21:50:23 +0200

    Testcommit

--- Python/commandhandler.py
+++ Python/commandhandler.py
@@ -25,6 +25,8 @@ def postmastodon(text):
 		os.system(f'toot post "{text}" -m "{FILE}"')
 	
 def postinstagram(text = ""):
+	''' ### this works fine, just didn't get instagrapi itself working on my serer yet
+	
 	client = Client()
 	with open("./Data/Instagram") as loginfile:
 			credentials = [line.rstrip() for line in loginfile]
@@ -34,6 +36,14 @@ def postinstagram(text = ""):
 	else:
 		client.photo_upload(FILE, caption=text)
 	print("posted on Instagram")
+	'''
+	print("*pretending like I am posting something on Instagram*")
+
+def posthtml(text):
+	HTMLdiv = f"<div class='message'>{text}</div>"
+    os.system(f'echo "{HTMLdiv}" >> "/home/signalticker/public_html/messages.html"')
+
+	
 
 def sendmessage(message, recipient = uid, file = ""):
 	if file == "":
@@ -49,7 +59,8 @@ def sendadmins(message, attachment):
 
 def post(posttext):
 	#postmastodon(posttext)
-	postinstagram(posttext)
+	#postinstagram(posttext)
+	posthtml(posttext)
 	#sendsublist(posttext)
 		
 def search(location, term):
@@ -60,10 +71,15 @@ def search(location, term):
 		else:
 			return 0
 
+#there is a builtin way to disable the bot temporarily
+with open("./Data/Enable") as file:
+	global enable
+	enable = file.read().strip() == "True"
+
 #checks if the author wants to send a command
 commandmode = 0
 prefix = "."
-if TEXT.startswith(prefix) and gid == "":
+if TEXT.startswith(prefix) and gid == "" and enable:
 	command = TEXT.lower()
 	command = command.split(" ",1)[0]
 	command = command[1:]
@@ -80,17 +96,27 @@ admin = search("./Data/Admins", uid)
 if admin:
 	print("Autormensch ist Admin!")
 
+if TEXT.startswith(f"{prefix}enable") and admin:
+	os.system(f"echo {not enable} > ./Data/Enable")
+	sendmessage(f"enable Status ist auf: {not enable}")
+	exit()
+
 #this is where the commands go
 if commandmode:
+		
 	if command == "ping":
 		sendmessage("pong")
-				
+			
 	if command == "help":
 		sendmessage(f'''$(printf "Hier ist eine Liste von commands:
 {prefix}subscribe fügt dich zu der Verteilerliste hinzu
 {prefix}unsubscribe entfernt dich von der Verteilerliste
 {prefix}ping ... find das am Besten selbst herraus :)
 {prefix}post postet einen Aufruf auf allen Kanälen")''')
+	
+	if command == "helphidden":
+		sendmessage(f'''$(printf "Hier ist eine Liste von geheimen commands:
+{prefix}enable aktiviert/deaktiviert den Bot (admin only)")''')
 				
 	if command == "subscribe":
 		if subscribed:
@@ -126,7 +152,9 @@ if commandmode:
 		
 else:
 	if gid == "":
-		sendmessage(f"Hi, ich bin der Demoticker Bot! Wenn du dich fragst, was ich alles kann, schick mir eine Nachricht mit dem Inhalt '{prefix}help' (ohne anführungszeichen)!")
-			
+		if admin:
+			sendmessage(f"Du bist Admin, aber hast keinen Befehl ausgewählt. Das Quick Repost Feature kommt bald, wurde aber noch nicht entwickelt. Hab also noch etwas Geduld :)")
+		else:
+			sendmessage(f"Hi, ich bin der Demoticker Bot! Wenn du dich fragst, was ich alles kann, schick mir eine Nachricht mit dem Inhalt '{prefix}help' (ohne anführungszeichen)!")
 	else:
 		print("message from group")
